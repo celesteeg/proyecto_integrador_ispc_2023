6959ba9b6736d17a27ececafbc5af403521110f9
pau_gomez <gomezlauro.paula@gmail.com>
2023-05-07 13:43:07 -0300

    quiero agregar parte del Footer

--- FRONTEND/TIENDA.DE.MASCOTAS/src/app/app.module.ts
+++ FRONTEND/TIENDA.DE.MASCOTAS/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './Componentes/footer/footer.component';
 
